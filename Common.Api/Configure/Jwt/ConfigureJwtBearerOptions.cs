ed6422b1a1434e6b747fddde3187aa764b1aa2b5
rtm0x143 <russia-artyom@mail.ru>
2023-04-03 22:37:06 +0700

    feat(Common.Api): jwt authntication configuration

---
+++ Common.Api/Configure/Jwt/ConfigureJwtBearerOptions.cs
@@ -0,0 +1,20 @@
+﻿using Common.Api.jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+
+namespace Common.Api.Configure.Jwt;
+
+public class ConfigureJwtBearerOptions : IConfigureOptions<JwtBearerOptions>
+{
+    private readonly IJwtValidator _validator;
+
+    public ConfigureJwtBearerOptions(IJwtValidator validator)
+    {
+        _validator = validator;
+    }
+
+    public void Configure(JwtBearerOptions options)
+    {
+        options.TokenValidationParameters = _validator.ValidationParameters;
+    }
+}
---
+++ Common.Api/Configure/Jwt/JwtBearerConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Common.Api.jwt;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Common.Api.Configure.Jwt;
+
+public static class JwtBearerConfigurationExtensions
+{
+    /// <summary>
+    /// Adds Jwt authentication, singleton <see cref="IJwtValidator"/> service and also configures <see cref="IOptions{T}"/> for <see cref="JwtConfigurationProperties"/>  
+    /// </summary>
+    /// <returns><see cref="AuthenticationBuilder"/> so you can chain and modify authentication</returns>
+    public static AuthenticationBuilder AddJwtBearerAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtConfigurationProperties>(
+            options => options.ReadConfiguration(configuration));
+        
+        services.AddSingleton<IJwtValidator, JwtValidator>();
+        services.AddTransient<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
+        return services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+    }
+}
